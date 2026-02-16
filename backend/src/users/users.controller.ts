c5ab34770ed87d8333639ba2e77f53afcaaef8a2
Peanutt47 <pnchalee@gmail.com>
2026-02-16 22:50:52 +0700

    fix: deployment setup

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, UseGuards, Req } from '@nestjs/common';
-import { AuthGuardService } from 'src/auth-guard/auth-guard.service';
+import { AuthGuardService } from '../auth-guard/auth-guard.service';
 
 @Controller('users')
 export class UsersController {
