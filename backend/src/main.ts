faf8e1fcefb1e7a3cd97ac72220c1d7339288eb4
Peanutt47 <pnchalee@gmail.com>
2026-02-16 22:47:26 +0700

    fix: deployment setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,15 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import 'dotenv/config';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  
   app.enableCors({
     origin: process.env.FRONTEND_URL ?? 'http://localhost:3100',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3100);
+  
+  if (process.env.NODE_ENV !== 'production') {
+    await app.listen(process.env.PORT ?? 3100);
+  } else {
+    await app.init();
+  }
 }
+
 bootstrap();
+
+export default server;
